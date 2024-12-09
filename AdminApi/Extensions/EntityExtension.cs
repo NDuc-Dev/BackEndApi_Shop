48c0c93888b986205af8a081de638e5c8dd670b4
NDuc <nguyenngocduc260504@gmail.com>
2024-12-09 16:48:58 +0700

    create entity extension

---
+++ AdminApi/Extensions/EntityExtension.cs
@@ -0,0 +1,21 @@
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Shared.Data;
+
+namespace WebIdentityApi.Extensions
+{
+    public static class EntityExtension
+    {
+        public static async Task<bool> IsExistsAsync<T>(this ApplicationDbContext context, string key, object value) where T : class
+        {
+            var parameter = Expression.Parameter(typeof(T), "x");
+            var propertyAccess = Expression.Property(parameter, key);
+            var constant = Expression.Constant(value);
+            var equalExpression = Expression.Equal(propertyAccess, constant);
+            var lambda = Expression.Lambda<Func<T, bool>>(equalExpression, parameter);
+
+            return await context.Set<T>().AnyAsync(lambda);
+        }
+
+    }
+}
