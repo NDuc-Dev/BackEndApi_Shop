9b909fb7f7a602cf32310a3e00d8944942a3699b
NDuc <nguyenngocduc260504@gmail.com>
2024-12-10 17:18:09 +0700

    create CreateProductDto

---
+++ AdminApi/DTOs/Product/CreateProductDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using AdminApi.DTOs.ProductColor;
+
+namespace AdminApi.DTOs.Product
+{
+    #nullable disable
+    public class CreateProductDto
+    {
+        [Required(ErrorMessage = "Product Name is required")]
+        public string ProductName { get; set; }
+        [Required(ErrorMessage = "Product Description is required")]
+        public string ProductDescription { get; set; }
+        public int BrandId { get; set; }
+        public List<int> NameTagId { get; set; }
+        public List<CreateProductColorDto> Variants { get; set; }
+    }
+}
