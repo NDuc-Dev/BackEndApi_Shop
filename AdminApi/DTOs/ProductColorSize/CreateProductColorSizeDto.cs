b2a81cd99353ddb5641fbc92f982d57199848b7a
NDuc <nguyenngocduc260504@gmail.com>
2024-12-10 17:18:59 +0700

    create CreateProducColorSizeDto

---
+++ AdminApi/DTOs/ProductColorSize/CreateProductColorSizeDto.cs
@@ -0,0 +1,8 @@
+namespace AdminApi.DTOs.ProductColorSize
+{
+    public class CreateProductColorSizeDto
+    {
+        public int SizeId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
