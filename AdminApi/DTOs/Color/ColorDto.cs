b3eca7a935c81e69209c906c2135f50c0f1b6de1
NDuc <nguyenngocduc260504@gmail.com>
2024-12-10 17:17:30 +0700

    create colorDto

---
+++ AdminApi/DTOs/Color/ColorDto.cs
@@ -0,0 +1,9 @@
+﻿namespace AdminApi.DTOs.Color
+{
+    #nullable disable
+    public class ColorDto
+    {
+        public int ColorId {get; set;}
+        public string ColorName { get; set; }
+    }
+}
