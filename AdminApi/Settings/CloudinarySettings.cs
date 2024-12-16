8d31bdfec5dd04bbf3bb42b510ab750b1fed6372
NDuc <nguyenngocduc260504@gmail.com>
2024-12-16 19:31:10 +0700

    create setting file, add setting class for cloudinary services

---
+++ AdminApi/Settings/CloudinarySettings.cs
@@ -0,0 +1,10 @@
+namespace AdminApi.Setting
+{
+    #nullable disable
+    public class CloudinarySettings
+    {
+        public string CloudName { get; set; }
+        public string ApiKey { get; set; }
+        public string ApiSecret { get; set; }
+    }
+}
