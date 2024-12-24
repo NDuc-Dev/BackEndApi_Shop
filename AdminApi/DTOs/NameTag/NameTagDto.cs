3b8f4c6cb1c408cdb09fc90f80b3f448a3850291
NDuc <nguyenngocduc260504@gmail.com>
2024-12-24 19:37:25 +0700

    update NameTagDto

--- AdminApi/DTOs/NameTag/NameTagDto.cs
+++ AdminApi/DTOs/NameTag/NameTagDto.cs
@@ -1,9 +1,7 @@
 namespace AdminApi.DTOs.NameTag
 {
-    #nullable disable
     public class NameTagDto
     {
         public int TagId { get; set; }
-        public string TagName { get; set; }
     }
 }
