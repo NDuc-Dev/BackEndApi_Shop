63edb2638f2b4dc6e09233372e4098dfbecd43c1
NDuc <nguyenngocduc260504@gmail.com>
2024-12-24 19:38:38 +0700

    update product color dto

--- AdminApi/DTOs/ProductColor/ProductColorDto.cs
+++ AdminApi/DTOs/ProductColor/ProductColorDto.cs
@@ -4,13 +4,11 @@ namespace AdminApi.DTOs.ProductColor
 {
     public class ProductColorDto
     {
-        public int? ProductColorId { get; set; }
+        public int? ProductColorId { get; set; } = null; 
         public int ProductId { get; set; }
         public int ColorId { get; set; }
-        public string? ColorName { get; set; }
         public decimal UnitPrice { get; set; }
         public List<string>? ImagePath { get; set; }
-        public List<string>? Images { get; set; }
         public List<ProductColorSizeDto>? ProductColorSize { get; set; }
     }
 }
