61e9f3f1993f63b79e9f0b323d97d938b07d8d13
NDuc <nguyenngocduc260504@gmail.com>
2024-12-27 19:09:06 +0700

    Create UnitOfWork repository

---
+++ Shared/Repositories/UnitOfWork.cs
@@ -0,0 +1,28 @@
+using Shared.Data;
+using Shared.Interfaces;
+
+namespace Shared.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task BeginTransactionAsync()
+        {
+            await _context.Database.BeginTransactionAsync();
+        }
+
+        public async Task CommitTransactionAsync()
+        {
+            await _context.Database.CommitTransactionAsync();
+        }
+
+        public async Task RollbackTransactionAsync()
+        {
+            await _context.Database.RollbackTransactionAsync();
+        }
+    }
+}
