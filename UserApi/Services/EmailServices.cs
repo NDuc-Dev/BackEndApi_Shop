88cab089b39134de2244e60639a18740d2dd4c84
NDuc <nguyenngocduc260504@gmail.com>
2024-12-28 12:56:31 +0700

    create email services and add base func

---
+++ UserApi/Services/EmailServices.cs
@@ -0,0 +1,35 @@
+using Mailjet.Client;
+using Mailjet.Client.TransactionalEmails;
+using UserApi.DTOs.Email;
+using UserApi.Interfaces;
+
+namespace UserApi.Services
+{
+    public class EmailServices : IEmailServices
+    {
+        private readonly IConfiguration _config;
+        public EmailServices(IConfiguration config)
+        {
+            _config = config;
+        }
+        public async Task<bool> SendEmail(EmailSendDto emailSend)
+        {
+            MailjetClient mailjet = new MailjetClient(_config["MailJet:APIKey"], _config["MailJet:SecretKey"]);
+            var email = new TransactionalEmailBuilder().WithFrom(new SendContact(_config["Email:From"], _config["Email:ApplicationName"]))
+                .WithSubject(emailSend.Subject)
+                .WithHtmlPart(emailSend.Body)
+                .WithTo(new SendContact(emailSend.To))
+                .Build();
+
+            var response = await mailjet.SendTransactionalEmailAsync(email);
+            if (response.Messages != null)
+            {
+                if (response.Messages[0].Status == "success")
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
