031cee4c5215d0398dbba9f58e5a06b35f66b806
NDuc <nguyenngocduc260504@gmail.com>
2024-12-28 12:54:12 +0700

    create Dto for Email

---
+++ UserApi/DTOs/Email/EmailSendDto.cs
@@ -0,0 +1,16 @@
+namespace UserApi.DTOs.Email
+{
+    public class EmailSendDto
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+
+        public EmailSendDto(string to, string subject, string body)
+        {
+            To = to;
+            Subject = subject;
+            Body = body;
+        }
+    }
+}
