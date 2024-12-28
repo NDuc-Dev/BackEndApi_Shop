08f3702512ba99de21958078c3e40e1cd1c7d6fe
NDuc <nguyenngocduc260504@gmail.com>
2024-12-28 12:57:04 +0700

    Create Jwt services

---
+++ UserApi/Services/JwtServices.cs
@@ -0,0 +1,56 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using UserApi.Interfaces;
+using Microsoft.IdentityModel.Tokens;
+using Shared.Data;
+using Shared.Models;
+
+namespace UserApi.Services
+{
+    public class JwtServices : IJwtServices
+    {
+        private readonly IConfiguration _config;
+        private SymmetricSecurityKey _jwtKey;
+        private readonly ApplicationDbContext _context;
+        public JwtServices(IConfiguration config, ApplicationDbContext context)
+        {
+            _context = context;
+            _config = config;
+            _jwtKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWT:Key"]!));
+
+        }
+
+        public string CreateJwt(User user, IList<string> userRole)
+        {
+            List<Claim> userClaims = GetClaim(user, userRole);
+            var credentials = new SigningCredentials(_jwtKey, SecurityAlgorithms.HmacSha256Signature);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(userClaims),
+                Expires = DateTime.UtcNow.AddDays(int.Parse(_config["JWT:ExpiresInDays"]!)),
+                SigningCredentials = credentials,
+                Issuer = _config["JWT:Issuer"]
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var jwt = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(jwt);
+        }
+
+        #region Private Helper Method
+        private List<Claim> GetClaim(User user, IList<string> userRole)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+            };
+            foreach (var role in userRole)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            return claims;
+        }
+        #endregion
+    }
+}
