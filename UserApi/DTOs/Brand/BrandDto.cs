350ca812bf15d6e71f87827dcbf4d808112aacd9
NDuc <nguyenngocduc260504@gmail.com>
2024-12-30 19:51:03 +0700

    add DTO for brand

---
+++ UserApi/DTOs/Brand/BrandDto.cs
@@ -0,0 +1,10 @@
+namespace UserApi.DTOs.Brands
+{
+    public class BrandDto
+    {
+        public int BrandId { get; set; }
+        public string? BrandName { get; set; }
+        public string? ImagePath { get; set; }
+        public string? Description { get; set; }
+    }
+}
